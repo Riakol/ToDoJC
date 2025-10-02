f175b81b20c06f2ed7035f2fdbceade72101272d
Riakol <inspi47@gmail.com>
2025-10-02 15:56:46 +0500

    add: Created a card item

--- app/src/main/java/com/riakol/todojc/presentation/groupScreen/GroupScreen.kt
+++ app/src/main/java/com/riakol/todojc/presentation/groupScreen/GroupScreen.kt
@@ -1,5 +1,6 @@
 package com.riakol.todojc.presentation.groupScreen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -15,10 +17,12 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.DensityMedium
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -29,12 +33,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import androidx.compose.runtime.collectAsState
+import com.riakol.todojs.R
 
 
 @Composable
@@ -45,6 +52,7 @@ fun GroupScreen(
 ) {
     var dialogState by remember { mutableStateOf<DialogTaskState>(DialogTaskState.None) }
     val tasksState by viewModel.tasks.collectAsStateWithLifecycle()
+    val groupDetailsState by viewModel.groupDetails.collectAsStateWithLifecycle()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -90,8 +98,14 @@ fun GroupScreen(
             }
 
             Spacer(modifier = Modifier.height(16.dp))
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Group name: " + viewModel.groupDetails.collectAsState().value?.name ?: "")
+            groupDetailsState?.name?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
             LazyColumn(
                 Modifier
                     .fillMaxSize()
@@ -99,7 +113,7 @@ fun GroupScreen(
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(tasksState) { task ->
-                    Text(text = task.title)
+                    TaskCardItem(taskTitle = task.title)
                 }
             }
         }
@@ -117,6 +131,7 @@ fun GroupScreen(
                 }
             )
         }
+
         is DialogTaskState.RenameTask -> TODO()
     }
 
@@ -163,10 +178,38 @@ fun AddNewTaskDialog(
 
 @Composable
 fun TaskCardItem(taskTitle: String) {
-    Row {
-        Column {
-            Card {
-                Text(text=taskTitle)
+    ElevatedCard(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+                TODO()
+            }
+        ) {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.circle_outline),
+                    contentDescription = "Task status",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            Column {
+                Text(
+                    text = "$taskTitle\n8 of 9",
+                    modifier = Modifier.padding(16.dp),
+                    textAlign = TextAlign.Start
+                )
             }
         }
     }
